dcf2cfe5fdb704ff35e6d2d50a9b024ad143e7b1
CrazyChr1504 <chris95504@gmail.com>
2022-04-08 09:29:06 +0200

    Initial Commit

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from resources import Word_lists, import_lists
+from resources import Three_Letter_Words, Four_Letter_Words, Five_Letter_Words, import_3list, import_4list, import_5list
 
 def main():
-    pass
+    t3lw = import_3list()
+    f4lw = import_4list()
+    f5lw = import_5list()
+    
+
 
 if __name__ == "__main__":
     main()
--- resources.py
+++ resources.py
@@ -1,35 +1,69 @@
 # Imports
 # Classes
-class Word_lists:
-    def __init__(self, three_letters : list, four_letters : list, five_letters : list):
-        self.three_letters = three_letters
-        self.four_letters = four_letters
-        self.five_letters = five_letters
-        
-    def get_three_letter(self):
-        return self.three_letters
-    
-    def get_four_letter(self):
-        return self.four_letters
 
-    def get_five_letter(self):
-        return self.five_letters
-# Functions
-def import_lists():
+class Three_Letter_Words:
+    def __init__(self, word_list3 : list):
+        self.word_list3 = word_list3
+
+    def get_word_list3(self):
+        return self.word_list3
+        
+    def add_words3(self):
+        print("How many words would you like to add to the word list?")
+        amount = input("Amount: ")
+        for _ in range(amount):
+            word = input("\nWhat word would you like to add?\nWord: ")
+            self.word_list3.append(word)
+        return self.word_list3
+
+class Four_Letter_Words:
+    def __init__(self, word_list4 : list):
+        self.word_list4 = word_list4
+
+    def get_word_list4(self):
+        return self.word_list4
+        
+    def add_words3(self):
+        print("How many words would you like to add to the word list?")
+        amount = input("Amount: ")
+        for _ in range(amount):
+            word = input("\nWhat word would you like to add?\nWord: ")
+            self.word_list4.append(word)
+        return self.word_list4
+
+class Five_Letter_Words:
+    def __init__(self, word_list5 : list):
+        self.word_list5 = word_list5
+
+    def get_word_list3(self):
+        return self.word_list5
+        
+    def add_words3(self):
+        print("How many words would you like to add to the word list?")
+        amount = input("Amount: ")
+        for _ in range(amount):
+            word = input("\nWhat word would you like to add?\nWord: ")
+            self.word_list5.append(word)
     
+        
+# Functions
+def import_3list():
+    t3lw = []
     with open("words_three_letters.txt","r",encoding="utf8") as f3:
-        t3lw = []
         for word in f3.readlines():
             t3lw.append(word)
+    return t3lw
     
+def import_4list():
+    f4lw = []
     with open("words_four_letters.txt","r",encoding="utf8") as f4:
-        f4lw = []
         for word in f4.readlines():
             f4lw.append(word)
-    
+    return f4lw
+
+def import_5list():
+    f5lw = []
     with open("words_five_letters.txt","r",encoding="utf8") as f5:
-        f5lw = []
         for word in f5.readlines():
             f5lw.append(word)
-        
-    Word_lists(t3lw,f4lw,f5lw)
+    return f5lw
